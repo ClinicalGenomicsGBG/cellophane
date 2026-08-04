aac669906bb3212b3383c3f0bbc6150635c775ac
dodslaser <2969909+dodslaser@users.noreply.github.com>
2026-08-04 16:22:03 +0200

    fix: Typing fixes for predicates

--- src/cellophane/modules/predicate.py
+++ src/cellophane/modules/predicate.py
@@ -8,27 +8,28 @@ if TYPE_CHECKING:
     from cellophane.cfg import Config
     from typing import TypeVar
 
-    S = TypeVar("S", bound=Sample)
+    SAMPLE = TypeVar("SAMPLE", bound=Sample)
+    SAMPLES = TypeVar("SAMPLES", bound=Samples)
 
 class SAMPLES_PREDICATE(Protocol):
-    def __call__(self, sample: Sample, /, samples: Samples, config: Config) -> bool: ...
+    def __call__(self, sample: SAMPLE, /, samples: SAMPLES, config: Config) -> bool: ...
 
 class EXCEPTION_PREDICATE(Protocol):
     def __call__(self, exception: BaseException, config: Config) -> bool: ...
 
-def select_samples(samples: Samples[S], config: Config, predicate: SAMPLES_PREDICATE) -> Samples[S]:
+def select_samples(samples: SAMPLES, config: Config, predicate: SAMPLES_PREDICATE) -> SAMPLES:
     """Selects samples based on a predicate function.
 
     Args:
     ----
-        samples (Samples[S]): The samples to be filtered.
+        samples (SAMPLES): The samples to be filtered.
         config (Config): The configuration object.
         predicate (PREDICATE): A predicate function that takes a sample, the samples, and the config,
             and returns True if the sample should be included in the result.
 
     Returns:
     -------
-        Samples[S]: A new Samples object containing the selected samples.
+        SAMPLES: A new Samples object containing the selected samples.
     """
     instance = deepcopy(samples)
     instance.data = [

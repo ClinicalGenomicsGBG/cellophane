7959ce2e12b1a7a4b0d6435d2ed460517ac6bd27
dodslaser <2969909+dodslaser@users.noreply.github.com>
2026-08-05 11:27:51 +0200

    fix: Ensure deprecation warnings are logged

--- src/cellophane/src.py
+++ src/cellophane/src.py
@@ -6,7 +6,7 @@ from warnings import warn
 warn(
     "Importing from cellophane.src will be deprecated in a future version of cellophane. "
     "References to 'cellophane.src' should be replaced with 'cellophane'",
-    category=PendingDeprecationWarning,
+    category=DeprecationWarning,
 )
 
 __path__ = [str(Path(__file__).parent)]
